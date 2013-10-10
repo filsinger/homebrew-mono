4c7acb1add2e2bbf5cb8ff872d16b1b7d31ebb20
Jason Filsinger <jason.filsinger@gmail.com>
2013-10-09 19:40:00 -0700

    updating to mono 3.2.3

--- mono.rb
+++ mono.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Mono < Formula
   homepage 'http://http://www.mono-project.com/'
-  url 'http://download.mono-project.com/sources/mono/mono-3.0.7.tar.bz2'
-  sha1 '0699c119f6aded3912797b45049932609020bc29'
+  url 'http://download.mono-project.com/sources/mono/mono-3.2.3.tar.bz2'
+  sha1 'e356280ae45beaac6476824d551b094cd12e03b9'
   head 'git://github.com/mono/mono.git'
 
   #depends_on :x11 # if your formula requires any X11/XQuartz components
@@ -16,7 +16,7 @@ class Mono < Formula
   def install
     args = ["--prefix=#{prefix}", "--with-glib=embedded", "--enable-nls=no"]
     args << "--host=x86_64-apple-darwin10" if MacOS.prefer_64_bit?
-    
+
     system "./configure", *args
     system "make"
     system "make install"
