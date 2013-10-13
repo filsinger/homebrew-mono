92c782aef97033094f1b1519af41c1771b82a616
Jason Filsinger <jason.filsinger@gmail.com>
2013-10-13 00:52:23 -0700

    Mono 3.2.3

--- mono.rb
+++ mono.rb
@@ -6,18 +6,15 @@ class Mono < Formula
   sha1 'e356280ae45beaac6476824d551b094cd12e03b9'
   head 'git://github.com/mono/mono.git'
 
-  #depends_on :x11 # if your formula requires any X11/XQuartz components
-
-  if build.head?
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-  end
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
 
   def install
     args = ["--prefix=#{prefix}", "--with-glib=embedded", "--enable-nls=no"]
     args << "--host=x86_64-apple-darwin10" if MacOS.prefer_64_bit?
 
-    system "./configure", *args
+    system "./autogen.sh", *args
     system "make"
     system "make install"
   end
